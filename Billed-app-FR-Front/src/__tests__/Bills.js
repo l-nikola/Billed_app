afc597b2b425cfb642408fb8cdda229cc0984fa2
l-nikola <legay.nikola.pro@gmail.com>
2025-06-03 20:44:14 +0200

    fix : add test Then I click on the eye icon, a modal should open

--- Billed-app-FR-Front/src/__tests__/Bills.js
+++ Billed-app-FR-Front/src/__tests__/Bills.js
@@ -5,6 +5,8 @@
 import { screen, waitFor } from "@testing-library/dom";
 import BillsUI from "../views/BillsUI.js";
 import { bills } from "../fixtures/bills.js";
+import Bills from "../containers/Bills.js";
+import userEvent from "@testing-library/user-event";
 import { ROUTES_PATH } from "../constants/routes.js";
 import { localStorageMock } from "../__mocks__/localStorage.js";
 
@@ -68,6 +70,40 @@ describe("Given I am connected as an employee", () => {
       newBillButton.click();
       const newBillModal = screen.getByTestId("form-new-bill");
       expect(newBillModal).toBeTruthy();
+    });
+
+    // Test that the eye icon opens a modal with the bill
+    test("Then I click on the eye icon, a modal should open", async () => {
+      Object.defineProperty(window, "localStorage", {
+        value: localStorageMock,
+      });
+      window.localStorage.setItem(
+        "user",
+        JSON.stringify({
+          type: "Employee",
+        })
+      );
+      const root = document.createElement("div");
+      root.setAttribute("id", "root");
+      document.body.append(root);
+      router();
+      window.onNavigate(ROUTES_PATH.Bills);
+      new Bills({
+        document,
+        onNavigate,
+        store: null,
+        localStorage: window.localStorage,
+      });
+      // Mock de la méthode .modal() de Bootstrap pour éviter une erreur en test
+      $.fn.modal = jest.fn();
+
+      const eyeIcons = screen.getAllByTestId("icon-eye");
+      userEvent.click(eyeIcons[0]);
+      expect($.fn.modal).toHaveBeenCalledWith("show");
+      const modal = document.querySelector("#modaleFile");
+      expect(modal).toBeTruthy();
+      const modalImg = modal.querySelector("img");
+      expect(modalImg).toBeTruthy();
     });
   });
 });
