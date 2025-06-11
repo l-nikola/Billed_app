bca652de61f37064507e9085a60ae84d78434bba
l-nikola <legay.nikola.pro@gmail.com>
2025-06-11 22:17:00 +0200

    feat : add integration test post

--- Billed-app-FR-Front/src/__tests__/NewBill.js
+++ Billed-app-FR-Front/src/__tests__/NewBill.js
@@ -118,3 +118,111 @@ describe("Given I am connected as an employee", () => {
     });
   });
 });
+
+// test d'intégration POST
+describe("When I navigate to employee page", () => {
+  describe("Given I am connected as an employee and I create new bill", () => {
+    test("Then employee create new bill", async () => {
+      const postSpy = jest.spyOn(mockStore, "bills");
+      const bill = {
+        id: "47qAXb6fIm2zOKkLzMro",
+        vat: "80",
+        fileUrl:
+          "https://firebasestorage.googleapis.com/v0/b/billable-677b6.a…f-1.jpg?alt=media&token=c1640e12-a24b-4b11-ae52-529112e9602a",
+        status: "pending",
+        type: "Hôtel et logement",
+        commentary: "séminaire billed",
+        name: "encore",
+        fileName: "preview-facture-free-201801-pdf-1.jpg",
+        date: "2004-04-04",
+        amount: 400,
+        commentAdmin: "ok",
+        email: "a@a",
+        pct: 20,
+      };
+      const postBills = await mockStore.bills().update(bill);
+      expect(postSpy).toHaveBeenCalledTimes(1);
+      expect(postBills).toStrictEqual(bill);
+    });
+
+    describe("When an error occurs on API", () => {
+      test("Add bill from an API and fail with a 404 error", async () => {
+        window.localStorage.setItem(
+          "user",
+          JSON.stringify({
+            type: "Employee",
+          })
+        );
+
+        const root = document.createElement("div");
+        root.setAttribute("id", "root");
+        document.body.appendChild(root);
+        router();
+        window.onNavigate(ROUTES_PATH.NewBill);
+
+        const postSpy = jest.spyOn(console, "error");
+
+        const store = {
+          bills: jest.fn(() => newBill.store),
+          create: jest.fn(() => Promise.resolve({})),
+          update: jest.fn(() => Promise.reject(new Error("404"))),
+        };
+
+        const newBill = new NewBill({
+          document,
+          onNavigate,
+          store,
+          localStorage,
+        });
+        newBill.isImgFormatValid = true;
+
+        // Submit form
+        const form = screen.getByTestId("form-new-bill");
+        form.addEventListener("submit", jest.fn((e) => newBill.handleSubmit(e)));
+
+        fireEvent.submit(form);
+        await new Promise(process.nextTick);
+        expect(postSpy).toBeCalledWith(new Error("404"));
+      });
+
+      test("Add bill from an API and fail with a 500 error", async () => {
+        window.localStorage.setItem(
+          "user",
+          JSON.stringify({
+            type: "Employee",
+          })
+        );
+
+        const root = document.createElement("div");
+        root.setAttribute("id", "root");
+        document.body.appendChild(root);
+        router();
+        window.onNavigate(ROUTES_PATH.NewBill);
+
+        const postSpy = jest.spyOn(console, "error");
+
+        const store = {
+          bills: jest.fn(() => newBill.store),
+          create: jest.fn(() => Promise.resolve({})),
+          update: jest.fn(() => Promise.reject(new Error("500"))),
+        };
+
+        const newBill = new NewBill({
+          document,
+          onNavigate,
+          store,
+          localStorage,
+        });
+        newBill.isImgFormatValid = true;
+
+        // Submit form
+        const form = screen.getByTestId("form-new-bill");
+        form.addEventListener("submit", jest.fn((e) => newBill.handleSubmit(e)));
+
+        fireEvent.submit(form);
+        await new Promise(process.nextTick);
+        expect(postSpy).toBeCalledWith(new Error("500"));
+      });
+    });
+  });
+});
